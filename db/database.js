26a3a7330a5b4d62ac942316e0c15d4f1eed70d9
HarshJ23 <hjawajiwar@gmail.com>
2023-07-01 17:49:15 +0530

    atlas

--- db/database.js
+++ db/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose" ;
 
 
 export const connectDB =()=>{
-    mongoose.connect("mongodb://127.0.0.1:27017" , {
+    mongoose.connect( process.env.MONGO_URI , {
         dbName : "finTrack",
     }).then(()=>console.log("mongoDB Database connected")).catch((e)=>console.log(e));
 }
