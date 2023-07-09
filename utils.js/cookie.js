cdb361ff733c93d6ea97a00e022b6408d170dbf8
HarshJ23 <hjawajiwar@gmail.com>
2023-07-09 16:22:54 +0530

    cookie handler resolve

--- utils.js/cookie.js
+++ utils.js/cookie.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken" ;
 
 
 export const sendCookie = (user,res,message,statusCode)=>{
-    const token = jwt.sign({_id : user._id} , "fintrackerv1");
+    const token = jwt.sign({_id : user._id} , process.env.JWT_TOKEN);
     res.status(statusCode).cookie("token" , token , {
         httpOnly : true ,
         maxAge : 15*60*1000, 
